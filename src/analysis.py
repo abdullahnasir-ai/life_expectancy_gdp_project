d9978d1928a1d1246a071cc0cf3bc215e8802a11
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-23 18:33:45 +0100

    Modelled using linear regression and fitted model

--- src/analysis.py
+++ src/analysis.py
@@ -9,4 +9,9 @@ import seaborn as sns
 r_coef = pearsonr(df['life_expectancy'], df['GDP'])
 # This statistic is 0.34320674844915594 meaning there is a moderate correlation between life expectancy and gdp.
 
+model = sm.OLS.from_formula("life_expectancy ~ GDP", data = df).fit()
+# This model tells us that for a GDP of 0 life expectancy is 7.005478e+01 which is hypothetical as there is no country with a GDP of 0, It also tells us that for every $1 in GDP Life expectancy increases by 7.047546e-13.
+
+fitted_values = model.predict(df)
+residuals = df.life_expectancy - fitted_values
 
