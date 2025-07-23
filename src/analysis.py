4db1b334ecf28dc835203624c7c1d862c305b1c6
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-23 18:33:16 +0100

    Fixed R coefficient call

--- src/analysis.py
+++ src/analysis.py
@@ -2,8 +2,11 @@ import pandas as pd
 from scipy.stats import pearsonr
 from data_loader import df
 import statsmodels
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-r_coef = pearsonr(df['Life expectancy at birth (years)'], df['GDP'])
+r_coef = pearsonr(df['life_expectancy'], df['GDP'])
 # This statistic is 0.34320674844915594 meaning there is a moderate correlation between life expectancy and gdp.
 
 
