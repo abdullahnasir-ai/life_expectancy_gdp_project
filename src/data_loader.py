142099325ffd802b90153bbfa22ad4083844bcef
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-23 18:32:48 +0100

    Renamed Columns
    Renamed life expectancy column to more easily access it

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('/Users/dully/PycharmProjects/life_expectancy_gdp_project/data/all_data.csv')
+df = df.rename(columns = {'Life expectancy at birth (years)': 'life_expectancy'})
 
-print(df.head())
+print(df)
