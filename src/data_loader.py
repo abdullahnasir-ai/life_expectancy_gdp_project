da85d4c311e7bd41625c903880d1b3938e8af7ec
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 15:50:51 +0100

    Edited dataframe
    Changed GDP to be in trillions

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,5 +3,6 @@ import pandas as pd
 df = pd.read_csv('/Users/dully/PycharmProjects/life_expectancy_gdp_project/data/all_data.csv')
 df = df.rename(columns = {'Life expectancy at birth (years)': 'life_expectancy'})
 df['Country'] = df['Country'].replace('United States of America', 'USA')
+df['GDP'] = df['GDP'] / 1e12
 
 print(df)
