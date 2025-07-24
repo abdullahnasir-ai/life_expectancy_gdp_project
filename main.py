760acd205a09560371dba384c83b6480b38e7e76
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 13:36:36 +0100

    Created figure of time vs gdp

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 from src.data_loader import df
-import src.data_loader
+import src.plotting as p
 
-plt.figure(figsize = (10, 8))
-for i in range(1, len(df.unique(df['Country'])) + 1):
-    country =
-    plt.subplot(2, 3, i)
-    timegdp
+plt.figure(figsize = (12, 10))
+for i in range(len(df['Country'].unique())):
+    country = df['Country'].unique()[i]
+    plt.subplot(2, 3, i + 1)
+    p.timegdp(country)
+    plt.subplots_adjust(wspace = 0.3, hspace = 0.3)
+plt.show()
+plt.clf()
 
