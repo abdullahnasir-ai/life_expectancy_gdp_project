8adaab60080cf334fbbd81a2692227e2e3410f0e
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 15:46:56 +0100

    Plotted a figure
    made a figure of GDP against time for countries

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import matplotlib.pyplot as plt
 from src.data_loader import df
 import src.plotting as p
 
-plt.figure(figsize = (12, 10))
+colors = plt.cm.tab10.colors
+
+plt.figure(figsize = (10, 8))
+for i in range(len(df['Country'].unique())):
+    country = df['Country'].unique()[i]
+    plt.subplot(2, 3, i + 1)
+    p.timegdp(country, colors[i % len(colors)])
+    plt.subplots_adjust(wspace = 0.4, hspace = 0.4)
+plt.show()
+plt.clf()
+
+plt.figure(figsize = (10, 8))
 for i in range(len(df['Country'].unique())):
     country = df['Country'].unique()[i]
     plt.subplot(2, 3, i + 1)
