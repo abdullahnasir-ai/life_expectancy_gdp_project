595b95481fa4f247b27573cdfde698c65dccea96
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 12:40:56 +0100

    Life expectancy against GDP plot function

--- src/plotting.py
+++ src/plotting.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 from data_loader import df
 
-def timegdp(country, color ):
+def timegdp(country, color):
     df_country = df[df['Country'] == country]
     plt.plot(df_country['Year'], df_country['GDP'], color = color, marker = 'o')
     plt.title(f"Country - {country}")
     plt.xlabel('Year')
     plt.ylabel('GDP in Trillions')
+
+def lifegdp(country, color):
+    df_country = df[df['Country'] == country]
+    plt.scatter(df_country['life_expectancy'], df_country['GDP'], color = color, marker = 'o')
+    plt.title(f"Country - {country}")
+    plt.xlabel('Life Expectancy')
+    plt.ylabel('GDP in Trillions')
+
