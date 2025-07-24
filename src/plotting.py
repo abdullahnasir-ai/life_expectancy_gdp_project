25b18de3b4f3fd39da8afd7177ea93c9cb39bad5
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 15:48:18 +0100

    Changed plotting functions
    Edited the plotting functions to include color as an argument

--- src/plotting.py
+++ src/plotting.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 from src.data_loader import df
 
-def timegdp(country):
+def timegdp(country, color):
     df_country = df[df['Country'] == country]
-    plt.plot(df_country['Year'], df_country['GDP'], marker = 'o')
+    plt.plot(df_country['Year'], df_country['GDP'],color = color, marker = 'o')
     plt.title(f"Country - {country}")
     plt.xlabel('Year')
     plt.ylabel('GDP in Trillions')
 
-def lifegdp(country):
+def lifegdp(country, color):
     df_country = df[df['Country'] == country]
-    plt.scatter(df_country['life_expectancy'], df_country['GDP'], marker = 'o')
+    plt.scatter(df_country['life_expectancy'], df_country['GDP'],color = color, marker = 'o')
     plt.title(f"Country - {country}")
     plt.xlabel('Life Expectancy')
     plt.ylabel('GDP in Trillions')
