9163d05b5fa23debce455eb38e25d4ac257b114c
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 13:04:08 +0100

    Removed color argument from functions

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Initial
+import matplotlib.pyplot as plt
+from src.data_loader import df
+import src.data_loader
+
+plt.figure(figsize = (10, 8))
+for i in range(1, len(df.unique(df['Country'])) + 1):
+    country =
+    plt.subplot(2, 3, i)
+    timegdp
+
--- src/plotting.py
+++ src/plotting.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 from data_loader import df
 
-def timegdp(country, color):
+def timegdp(country):
     df_country = df[df['Country'] == country]
-    plt.plot(df_country['Year'], df_country['GDP'], color = color, marker = 'o')
+    plt.plot(df_country['Year'], df_country['GDP'], marker = 'o')
     plt.title(f"Country - {country}")
     plt.xlabel('Year')
     plt.ylabel('GDP in Trillions')
 
-def lifegdp(country, color):
+def lifegdp(country):
     df_country = df[df['Country'] == country]
-    plt.scatter(df_country['life_expectancy'], df_country['GDP'], color = color, marker = 'o')
+    plt.scatter(df_country['life_expectancy'], df_country['GDP'], marker = 'o')
     plt.title(f"Country - {country}")
     plt.xlabel('Life Expectancy')
     plt.ylabel('GDP in Trillions')
