0e4afd61249d8e77988b530c4250dde8b18ea0b7
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-07-24 15:48:50 +0100

    Edited dataframe
    Changed column title of USA to be more easily accesible

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('/Users/dully/PycharmProjects/life_expectancy_gdp_project/data/all_data.csv')
 df = df.rename(columns = {'Life expectancy at birth (years)': 'life_expectancy'})
+df['Country'] = df['Country'].replace('United States of America', 'USA')
 
 print(df)
